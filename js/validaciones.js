4859ac134e1ca887f5d28ceae6e173d7f1a3f5d4
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2021-12-03 17:00:34 -0300

    cambio la validación del input de Cantidad

--- js/validaciones.js
+++ js/validaciones.js
@@ -18,7 +18,7 @@ export function campoRequerido(input){
 
 export function validarNumeros(input){
     //1.- crea una EXPRESIÓN REGULAR
-    let patron = /^[1-9]{1,2}$/;
+    let patron = /^[0-9]{1,2}$/;
     //probar que la Expresión regular funcione
     if(patron.test(input.value)){
         //si cumple la expresión regular
